be69e922952576fb05a4cf85a809dda881885c20
ziyad00 <ziyad.alotaibe@gmail.com>
2020-12-31 14:21:30 +0300

    df

--- api/urls.py
+++ api/urls.py
@@ -24,7 +24,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Instgram API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
